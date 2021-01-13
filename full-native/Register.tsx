62610f178c1cced367015187b2885a4b19363c09
michaelseg11 <michaelseg11@gmail.com>
2021-01-13 13:49:23 +0200

    register

--- full-native/Register.tsx
+++ full-native/Register.tsx
@@ -2,14 +2,18 @@ import React, { useState } from 'react';
 import UserUploader from './UserUploader';
 
 import { StackNavigationProp } from '@react-navigation/stack';
+import { useLogin } from '@hilma/auth-native';
 
 
 const Register: React.FC<{ navigation: StackNavigationProp<any> }> = ({ navigation }) => {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
 
-    const handleSend = () => {
+    const login = useLogin();
 
+    const handleSend = async () => {
+        await login("/signUp", { username, password });
+        navigation.navigate("cats");
     }
 
     const handleRedirect = () => {
